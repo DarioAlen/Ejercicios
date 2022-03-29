7dbb711e9ae0fc4952fb5203c977293c166cfdd1
Dario <darioeleven@gmail.com>
2022-03-29 16:45:41 -0300

    Ok

---
+++ AgendaContactoV1/AgendaContactoV1/Contacto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaContactoV1
+{
+    class Contacto
+    {
+        private int _codigo = 100;
+        private string _nombre;
+        private string _apellido;
+        private string _telefono;
+        private string _direccion;
+        private DateTime _fechaNacimiento;
+        private int _llamadas;
+
+        public Contacto(string nombre, string apellido, string telefono, string direccion, DateTime fechaNacimiento)
+        {
+            _codigo = AutoIncrementar();
+            _nombre = nombre;
+            _apellido = apellido;
+            _telefono = telefono;
+            _direccion = direccion;
+            _fechaNacimiento = fechaNacimiento;
+
+        }
+
+        public int Codigo
+        {
+            get
+            {
+                return _codigo;
+            }
+        }
+        
+        public int Edad()
+        {
+            double edad = (DateTime.Now - _fechaNacimiento).Days / 365;
+            return Convert.ToInt32(edad);
+        }
+
+        public void llamar()
+        {
+            _llamadas++;
+        }
+
+        public int AutoIncrementar()
+        {
+            return _codigo = _codigo + 1;
+        }
+
+        public int MostrarAgenda()
+        {
+            return _codigo; //+  _nombre.ToString() + "/n" + _apellido.ToString() + "/n" + _telefono + "/n" + _direccion + _fechaNacimiento.ToString();
+        }
+    }
+}
---
+++ AgendaContactoV1/AgendaContactoV1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AgendaContactoV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Contacto Juan = new Contacto("Juan","Perez", "1198785634", "Calle 1 2345",DateTime.Now.AddYears(-35));
+            Contacto Clara = new Contacto("Clara", "Argenti", "1143234234", "Calle ilusoria 42", DateTime.Now.AddYears(-34));
+            Contacto Maria = new Contacto("María", "Lopez", "1178463792", "Calle x 123", DateTime.Now.AddYears(-32));
+            Contacto Pedro = new Contacto("Pedro", "Gimeneh", "1124324435", "Calle y 323", DateTime.Now.AddYears(-28));
+
+            Agenda agendaconcreta = new Agenda("Agenda", "electrónica", 23);
+
+            agendaconcreta.AgregarContacto(Juan);
+            agendaconcreta.AgregarContacto(Clara);
+            agendaconcreta.AgregarContacto(Maria);
+            agendaconcreta.AgregarContacto(Pedro);
+
+            Console.WriteLine(agendaconcreta.ListadoDeContactos());
+
+        }
+    }
+}
